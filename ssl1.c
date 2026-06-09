876037d31f1e2f12d8f6c026b2214120775f284c
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-06-09 05:24:59 +0000

    update

---
+++ ssl1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+struct ssl
+{
+    int d;
+    struct ssl*link;
+};
+int main()
+{
+    struct ssl head,fst,nd,tail;
+    head.d=9;
+    fst.d=3;
+    nd.d=23;
+    tail.d=9;
+    head.link=&fst;
+    fst.link=&nd;
+    nd.link=&tail;
+    tail.link=NULL;
+    struct ssl *ptr;
+    ptr=&head;
+
+    
+    while(ptr!=NULL)
+    {
+        printf("%d\n",ptr->d);
+        ptr=ptr->link;
+    }
+
+}
