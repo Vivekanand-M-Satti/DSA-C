a64c5b174b27d2c402bd879f261863f100af1606
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-06-12 12:23:46 +0000

    update

---
+++ STACK/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int strt=0,end=0;
+    int size;
+    int arr[1000];
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+        end++;
+    }
+    
+
+}
