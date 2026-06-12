c1ddbb2058361c97e384312538e5177b236f78da
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-06-12 12:42:16 +0000

    update

--- STACK/1.c
+++ STACK/1.c
@@ -1,14 +1,29 @@
-#include<stdio.h>
-int main()
-{
-    int strt=0,end=0;
-    int size;
-    int arr[1000];
-    for(int i=0;i<size;i++)
-    {
-        scanf("%d",&arr[i]);
-        end++;
-    }
+//stack by using the array;
+// #include<stdio.h>
+// int main()
+// {
+//     int strt=0,end=0;
+//     int size;
+//     int arr[1000];
+//     for(int i=0;i<1000;i++)
+//     {
+//         scanf("%d",&arr[i]);
+//         end++;
+//     }
     
 
+// }
+
+#include<stdio.h>
+struct stack
+{
+    int top;
+    int capacity;
+    int *arr;
+};
+
+int main()
+{
+    struct stack s1;
+    
 }
